f827a1f65c1a0b95768a6f76a0f4725cfaef537e
Nikita Davis <ndavis28@wisc.edu>
2024-10-31 12:21:02 -0500

    Added add_transaction, record_payment, and change_password files

---
+++ app/src/main/java/com/cs407/squareup/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.cs407.squareup
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.add_transaction)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.addTransaction)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        val customSplit= findViewById<Button>(R.id.customSplit)
+        customSplit.setOnClickListener {
+
+        }
+        val equalSplit= findViewById<Button>(R.id.equalSplit)
+        equalSplit.setOnClickListener {
+
+        }
+        val useCamera= findViewById<Button>(R.id.useCamera)
+        useCamera.setOnClickListener {
+
+        }
+
+        val recordPayment= findViewById<Button>(R.id.recordPayment)
+        recordPayment.setOnClickListener {
+
+        }
+        val notifications= findViewById<CheckBox>(R.id.notifications)
+        notifications.setOnClickListener {
+
+        }
+        val logout= findViewById<Button>(R.id.logout)
+        logout.setOnClickListener {
+
+        }
+    }
+
+}
