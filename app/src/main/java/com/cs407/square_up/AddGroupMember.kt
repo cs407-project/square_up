fa017d0401f3f7ee3772ada2cc7743c3b9c044f4
Ari-Wisc <anosonovsky@wisc.edu>
2024-11-06 12:11:46 -0600

    added code so clicking the button just returns to previos page

--- app/src/main/java/com/cs407/square_up/AddGroupMember.kt
+++ app/src/main/java/com/cs407/square_up/AddGroupMember.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
+import android.widget.TextView
 
 
 class AddGroupMember : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_group_member)
+        val addMemberButton = findViewById<Button>(R.id.addMemberButton)
+        addMemberButton.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/cs407/square_up/CreateGroup.kt
+++ app/src/main/java/com/cs407/square_up/CreateGroup.kt
@@ -10,5 +10,9 @@ class CreateGroup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.create_group)
+        val CreateGroupButton = findViewById<Button>(R.id.CreateGroupButton)
+        CreateGroupButton.setOnClickListener {
+            finish()
+        }
     }
 }
