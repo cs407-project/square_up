80982d42d2c3b28039bab64d21c5e8ece7ee9b75
sanvikumar22 <skumar228@wisc.edu>
2024-11-06 11:08:19 -0600

    commit

--- app/src/main/java/com/cs407/square_up/AddGroupMember.kt
+++ app/src/main/java/com/cs407/square_up/AddGroupMember.kt
@@ -1,23 +1,14 @@
-
 package com.cs407.square_up
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
-import androidx.fragment.app.Fragment
-//import androidx.navigation.fragment.findNavController
-import com.cs407.square_up.R
 
-class AddGroupMember : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.add_group_member, container, false)
-
-        return view
+class AddGroupMember : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_group_member)
     }
 }
